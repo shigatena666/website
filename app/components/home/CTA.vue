9a59d73e75e51def589882e97b555839b43e9eb3
shigatena666 <github@shigatena.dev>
2024-09-30 21:52:03 +0200

    default commit

---
+++ app/components/home/CTA.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center gap-4 sm:gap-2">
+    <div class="flex flex-col items-center justify-center gap-4 sm:flex-row sm:gap-2">
+      <UTooltip
+        :text="$t('global.email')"
+        :shortcuts="['⌘', 'O']"
+      >
+        <SpotlightButton>
+          <NuxtLink
+            class="white-gradient relative flex items-center justify-center gap-2 transition-all duration-200"
+            to="/contact"
+          >
+            {{ $t("global.contact") }}
+            <UIcon
+              name="heroicons-envelope"
+              class="size-5 text-white/80"
+              aria-hidden="true"
+            />
+          </NuxtLink>
+        </SpotlightButton>
+      </UTooltip>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ app/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div>
+      <SettingsLanguageToggle class="fixed bottom-20 right-3 z-50 sm:bottom-4" />
+      <LayoutNavbar class="fixed bottom-5 z-50 flex" />
+      <div class="sm:mt-[50px]">
+        <slot />
+      </div>
+      <LayoutFooter />
+    </div>
+  </div>
+</template>
